8c1183b6f19b4d7895eda63481ddf456db5099fb
Ze Xuan Ong <ong.zexuan@gmail.com>
2019-06-27 16:06:23 +0800

    Added autoincrement for jobscentral crawler

--- sgjobs/spiders/jobscentral.py
+++ sgjobs/spiders/jobscentral.py
@@ -23,6 +23,7 @@ class JobscentralSpider(scrapy.Spider):
             for entry in data:
                 yield scrapy.Request(self.job_url.format(entry['id']), callback=self.parse_posting)
 
+            JobscentralSpider.current_page += 1
             #if JobscentralSpider.default_max_limit < 0 or JobscentralSpider.current_page <= self.default_max_limit:
             yield scrapy.Request(self.base_url.format(JobscentralSpider.current_page, 
                                                       JobscentralSpider.results_per_page), 
